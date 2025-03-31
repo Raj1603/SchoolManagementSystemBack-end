561c8e56031b5c6661be36c15bbc0189b3b74a01
Raj1603 <rajiyamuna1603@gmail.com>
2025-03-31 20:23:22 +0530

    Project uploaded on git repositoy

---
+++ Backend/Backend/src/test/java/com/SchoolManagementSystem/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SchoolManagementSystem.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
